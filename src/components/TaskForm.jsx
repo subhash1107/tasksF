34dc959b49a0f6d6a44379440b6bd3a607a16fb5
Subhash Satyarthi <120436794+subhashprogrammer@users.noreply.github.com>
2025-06-14 08:16:49 +0530

    styled task form

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -4,14 +4,14 @@ import Button from "./Button";
 const TaskForm = ({ onSubmit, editingTask }) => {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
-
+   
   useEffect(() => {
     if (editingTask) {
       setName(editingTask.name);
       setDescription(editingTask.description);
     }
   }, [editingTask]);
-
+  
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!name || !description) return alert("All fields required");
@@ -22,9 +22,23 @@ const TaskForm = ({ onSubmit, editingTask }) => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input value={name} onChange={(e) => setName(e.target.value)} placeholder="Task Name" />
-      <input value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Description" />
+        <div className="flex gap-3 w-full justify-between mb-2">
+      <input
+      className='px-4 py-2 rounded-md border w-70 bg-white'
+      type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Task Name"
+      />
       <Button type="submit">{editingTask ? "Update" : "Add"} Task</Button>
+      </div>
+      <input
+      className='px-4 py-2 rounded-md border w-full mb-2 bg-white'
+      type="text"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        placeholder="Description"
+      />
     </form>
   );
 };
